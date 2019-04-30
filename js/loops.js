6e5331c366ba492a49c27e17417864217bf136d2
unknown <mjanayvarela@gmail.com>
2019-04-29 20:57:49 -0700

    learning about loops

---
+++ js/loops.js
@@ -0,0 +1,23 @@
+var fruits = ['avocado', 'nectarine', 'mango', 'peach', 'banana'];
+console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+//for loop 
+
+// for(var i = 0; i < fruits.length; i++) {
+//     console.log(i);
+//     console.log(fruits[i]);
+// }
+
+
+// do while
+
+var index = 7;
+do {
+console.log(fruits[index]);
+index++;
+} while (index < fruits.length);
