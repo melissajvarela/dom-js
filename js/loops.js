939689105b11af3d216a716cb148fe9012008701
unknown <mjanayvarela@gmail.com>
2019-04-30 18:47:58 -0700

    learning while, for in and for of lopps

--- js/loops.js
+++ js/loops.js
@@ -16,8 +16,31 @@ console.log(fruits);
 
 // do while
 
-var index = 7;
-do {
+var index = 0;
+// do {
+// console.log(fruits[index]);
+// index++;
+// } while (index < fruits.length);
+
+// while
+
+while(index < fruits.length) {
 console.log(fruits[index]);
 index++;
-} while (index < fruits.length);
+// index += 1;
+}
+
+//while(hour < 18 || hour < 21) two straight lines stands for or
+// while (hour < 18 && hour < 21) {
+
+// }
+
+// for in 
+fruits.foo = 'test';
+for (var i in fruits) {
+console.log(i);
+}
+
+for (var i of fruits) {
+    console.log(i);
+}
